f5a8770566d371a5bf4dd09034abfa945aa1b720
web dev <rublin@gmx.de>
2022-11-13 16:12:19 +0100

    feat: add dotenv

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import { Bot } from "grammy";
+import dotenv from 'dotenv';
+const result = dotenv.config();
 
-const bot = new Bot(""); // <-- put your bot token here (https://t.me/BotFather)
+if (result.error) {
+  throw result.error
+}
+
+const bot = new Bot(result.parsed.TOKEN); // <-- put your bot token here (https://t.me/BotFather)
 
 // Reply to any message with "Hi there!".
-bot.on("message", (ctx) => ctx.reply("Hi there!"));
+bot.on("message", (ctx) => ctx.reply("Hi with dotenv!"));
 
 bot.start();
